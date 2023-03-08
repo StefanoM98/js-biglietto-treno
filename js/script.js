935dc7ef0abf9fbc5af1c4cfaba34cd7a84519bb
StefanoM98 <smartino030@gmail.com>
2023-03-08 13:34:37 +0100

    Add: street and userAge in number

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // CHIEDERE AL CLIENTE IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE E L'ETA'
 
-// CHILOMETRI CHE IL CLIENTE VORREBBE PERCORRERE
-const street = prompt ("Quanti chilometri vuoi percorrere?");
-// ETA' DEL CLIENTE
-const userAge = prompt ("Quanti anni hai? (in base alla tua età avrai uno sconto)")
+// CHILOMETRI CHE IL CLIENTE VORREBBE PERCORRERE TRASFORMANDO IL DATO INSERITO IN NUMERO
+const street = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
+// ETA' DEL CLIENTE TRASFORMANDO IL DATO INSERITO IN NUMERO
+const userAge = parseInt (prompt ("Quanti anni hai? (in base alla tua età avrai uno sconto)"))
+console.log(street, userAge);
