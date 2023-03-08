f0776e2545e752ee2f19c9e0c2bd82f0abc88f1d
StefanoM98 <smartino030@gmail.com>
2023-03-08 15:27:38 +0100

    Add: fixed and add bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 // CHIEDERE AL CLIENTE IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE E L'ETA'
+alert("Benvenuto in Boolean trainline. Con questo programma puoi calcolare il prezzo del tuo biglietto in base ai chilometri che vuoi percorrere e all'età che hai!!")
 
 // CHILOMETRI CHE IL CLIENTE VORREBBE PERCORRERE TRASFORMANDO IL DATO INSERITO IN NUMERO
-const street = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
+const kilometres = parseInt (prompt ("Quanti chilometri vuoi percorrere?"));
 // ETA' DEL CLIENTE TRASFORMANDO IL DATO INSERITO IN NUMERO
 const userAge = parseInt (prompt ("Quanti anni hai? (in base alla tua età avrai uno sconto)"))
-console.log(street, userAge);
+console.log(kilometres, userAge);
+
+if (isNaN(kilometres && userAge)){
+    document.getElementById("error").innerHTML = "Non hai inserito i dati corretti. Ricarica la pagina premendo il tasto in alto del tuo browser oppure premi F5"
+} else {
 
 // CALCOLARE IL PREZZO DEL BIGLIETTO IN BASE AI CHILOMETRI SCELTI
-const price = street * 0.21;
+const price = kilometres * 0.21;
 console.log(price);
 
 // DEFINIAMO GLI SCONTI IN BASE ALL'ETA' DEL CLIENTE
@@ -16,12 +21,16 @@ const overAge = 0.60;
 // Calcoliamo il prezzo in base all'età del cliente
 let finalPrice = ""
 
-if (userAge < 18) {
+if (userAge <= 18) {
     finalPrice = (price * underAge);
-} else if (userAge > 65) {
+} else if (userAge >= 65) {
     finalPrice = (price * overAge)
 } else {
     finalPrice = price
 }
-
 console.log(finalPrice);
+
+// STAMPIAMO IL PREZZO DEL BIGLIETTO
+document.getElementById("final-price").innerHTML = `Il prezzo del tuo biglietto è: €${finalPrice}`
+
+}
