a483647410a8ff141ef061161085c37dd3064021
StefanoM98 <smartino030@gmail.com>
2023-03-08 13:30:48 +0100

    Add: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// CHIEDERE AL CLIENTE IL NUMERO DI CHILOMETRI CHE VUOLE PERCORRERE E L'ETA'
+
+// CHILOMETRI CHE IL CLIENTE VORREBBE PERCORRERE
+const street = prompt ("Quanti chilometri vuoi percorrere?");
+// ETA' DEL CLIENTE
+const userAge = prompt ("Quanti anni hai? (in base alla tua età avrai uno sconto)")
